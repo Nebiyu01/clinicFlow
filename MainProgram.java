b147d1638dc9a877bd4ec29e41b83965b7c4a34c
laiEEEEEEE <97525684+laiEEEEEEE@users.noreply.github.com>
2025-12-12 14:17:27 -0500

    Update MainProgram.java
    Added a login instruction

--- MainProgram.java
+++ MainProgram.java
@@ -1,5 +1,20 @@
 import javax.swing.SwingUtilities;
 
+/**
+ * ==========================
+ * How to Run ClinicFlow
+ * ==========================
+ *
+ * Default login:
+ * - Open the file staff.txt in the project folder.
+ * - it contains one of the accounts:
+ *        admin;password
+ * - Use that username(admin) and password(password) on the login screen to log in.
+ *
+ * After logging in, you can use the Schedule Appointment and View / Cancel tabs
+ * to test the system.
+ */
+
 public class MainProgram {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
